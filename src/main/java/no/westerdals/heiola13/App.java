da28a761eb6247d47d98a8dd599b21d17d6ac90e
HeiOla13 <heiola13@student.westerdals.no>
2015-10-05 12:28:51 +0200

    start
    start

---
+++ src/main/java/no/westerdals/heiola13/App.java
@@ -0,0 +1,13 @@
+package no.westerdals.heiola13;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
